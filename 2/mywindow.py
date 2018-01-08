ac2cc0b95c69bdf7ccd868e21da28330fa713412
dawang <854266726@qq.com>
2018-01-08 10:22:47 +0800

    test2

---
+++ 2/mywindow.py
@@ -0,0 +1,31 @@
+#from PyQt5 import QtWidgets  
+#import test  
+#import sys
+ 
+#class mywindow(QtWidgets.QWidget):  
+#     def __init__(self):  
+#         super(mywindow,self).__init__()  
+#         self.new=test.Ui_MainWindow()  
+#         self.new.setupUi(self)  
+#  
+#if __name__=="__main__":  
+#     app=QtWidgets.QApplication(sys.argv)  
+#     myshow=mywindow()  
+#     myshow.show()  
+#     sys.exit(app.exec_())  
+     
+import sys      
+from PyQt5 import QtWidgets  
+from ui import Ui_MainWindow  
+  
+class mywindow(QtWidgets.QWidget,Ui_MainWindow):  
+    def __init__(self):  
+        super(mywindow,self).__init__()  
+        self.setupUi(self)  
+  
+if __name__=="__main__":  
+  
+    app=QtWidgets.QApplication(sys.argv)  
+    myshow=mywindow()  
+    myshow.show()  
+    sys.exit(app.exec_())  
--- 3/mywindow.py
+++ 3/mywindow.py
@@ -6,21 +6,19 @@ Created on Sun Jan  7 15:50:14 2018
 """
 
 from PyQt5 import QtWidgets    
-from test3 import Ui_MainWindow   
+from ui import Ui_MainWindow   
 import sys   
 """点击按钮，在控制台输出helloworld"""    
-class mywindow(QtWidgets.QWidget,Ui_MainWindow):    
+class MyWindow(QtWidgets.QWidget,Ui_MainWindow):    
     def __init__(self):    
-        super(mywindow,self).__init__()    
+        super(MyWindow,self).__init__()    
         self.setupUi(self)  
         self.Button1.clicked.connect(self.myPrint)   #槽函数不用加括号  
     def myPrint(self):                               #定义槽  
         print("hello dawang!")  
     
 if __name__=="__main__":    
-
-    
     app=QtWidgets.QApplication(sys.argv)    
-    myshow=mywindow()    
+    myshow=MyWindow()    
     myshow.show()    
     sys.exit(app.exec_())  
